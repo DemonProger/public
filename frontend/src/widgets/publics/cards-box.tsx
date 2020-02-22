10c598e2111cf5e7b0fdb8747eb369250fac67ff
Dmitry <amld1998@mail.ru>
2020-02-22 22:21:44 +0300

    cards refactoring

--- frontend/src/widgets/publics/cards-box.tsx
+++ frontend/src/widgets/publics/cards-box.tsx
@@ -2,8 +2,9 @@
  
 import * as React from "react"
 import { CardDeck } from 'react-bootstrap'
-import PublicCard from './public-card'
+import PublicCard from '../templates/cards/public-card'
 import Styles from './styles.module.css'
+import { connect } from 'react-redux'
 
 
 export const CardsBox = () => {
@@ -11,7 +12,7 @@ export const CardsBox = () => {
   let items : any [] = []
   for (let i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
     items.push((
-      <PublicCard key={i}/>
+      <PublicCard key={i} title="test title" text="test text"/>
     ))
 
   return (
@@ -24,4 +25,18 @@ export const CardsBox = () => {
 }
 
 
-export default CardsBox
+/// export default CardsBox
+
+const mapStateToProps = (state: any) => {
+  return {
+      
+  }
+}
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+     
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardsBox)
---
+++ frontend/src/widgets/templates/cards/public-card.tsx
@@ -0,0 +1,31 @@
+ 
+
+import * as React from "react"
+import { Card, Col } from 'react-bootstrap'
+import Styles from './styles.module.css'
+
+
+type PublicCartProps = {
+  title: string, 
+  text: string, 
+  key: number
+}
+
+
+export const PublicCard = (props: PublicCartProps) => {
+  return (
+    <Col xs={12} sm={4}>
+      <Card className={Styles.Card}>
+        <Card.Body className={Styles.CardBody}>
+          <Card.Title className={Styles.CardTitle}>{props.title}</Card.Title>
+          <Card.Text className={Styles.CardText}>{props.text}</Card.Text>
+        </Card.Body>  
+      </Card>
+    </Col>
+  )
+}
+
+export default PublicCard
+
+
+
