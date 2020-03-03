fe275a63772d0b4988cbedcf224892ba0ba86b00
Dmitry <amld1998@mail.ru>
2020-03-03 23:02:41 +0300

    orm schema creation test

--- backend/build/create-schema.js
+++ backend/build/create-schema.js
@@ -7,8 +7,11 @@ exports.default = () => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     const orm = yield mikro_orm_1.MikroORM.init({
         entities: [user_1.User],
         dbName: process.env.DB_NAME || "db name is not setted in .env file",
-        clientUrl: 'postgresql://localhost:5432',
-        type: 'postgresql'
+        password: process.env.DB_PASSWORD || "password is not setted in .env file",
+        type: 'postgresql',
+        host: process.env.DB_HOST || "localhost",
+        port: parseInt("" + process.env.DB_PORT) || 5432,
+        user: process.env.DB_USER || "postgres"
     });
     const generator = orm.getSchemaGenerator();
     const dropDump = yield generator.getDropSchemaSQL();
@@ -19,8 +22,6 @@ exports.default = () => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     console.log(updateDump);
     const dropAndCreateDump = yield generator.generate();
     console.log(dropAndCreateDump);
-    yield generator.dropSchema();
-    yield generator.createSchema();
     yield generator.updateSchema();
     yield orm.close(true);
 });
--- backend/src/create-schema.ts
+++ backend/src/create-schema.ts
@@ -6,9 +6,13 @@ import { User } from './models/user'
 export default async () => {
   const orm = await MikroORM.init({
     entities: [User],
-    dbName: process.env.DB_NAME || "db name is not setted in .env file",           
-    clientUrl: 'postgresql://localhost:5432',
-    type: 'postgresql'    
+    dbName: process.env.DB_NAME || "db name is not setted in .env file", 
+    password: process.env.DB_PASSWORD || "password is not setted in .env file",           
+    // clientUrl: 'postgresql://localhost:5432',
+    type: 'postgresql', 
+    host: process.env.DB_HOST || "localhost", 
+    port: parseInt("" + process.env.DB_PORT) || 5432,  
+    user: process.env.DB_USER || "postgres"
   })
 
   const generator = orm.getSchemaGenerator()
@@ -27,8 +31,8 @@ export default async () => {
   console.log(dropAndCreateDump)
 
   // or you can run those queries directly, but be sure to check them first!
-  await generator.dropSchema()
-  await generator.createSchema()
+  // await generator.dropSchema()
+  // await generator.createSchema()
   await generator.updateSchema()
 
   await orm.close(true)
