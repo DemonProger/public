9b76ec8527f556d4d7ccf0385da6b386e7f64ef9
Dmitry <amld1998@mail.ru>
2020-03-10 20:18:53 +0300

    Revert "swagger updated"
    This reverts commit 1535c45204020ab260ea654899d94536d0a5e8f1.

--- shared/deploy.py
+++ shared/deploy.py
@@ -15,14 +15,6 @@ def copyFolder(src, dst, symlinks=False, ignore=None):
             shutil.copy2(s, d)
 
 
-def removeFolderContent(folder):
-    for root, dirs, files in os.walk(folder):
-        for f in files:
-            os.unlink(os.path.join(root, f))
-        for d in dirs:
-            shutil.rmtree(os.path.join(root, d))
-
-
 # configs
 FRONTEND_BUILD_FOLDER = "../frontend/build"
 BACK_PUBLIC_FOLDER = "../backend/public"
@@ -30,38 +22,36 @@ SITE_URL = "http://localhost:3000"
 
 # tasks
 BUILD_FRONTEND = True
-RUN_DEV_SERVER = False # True
+RUN_DEV_SERVER = True
 COPY_FRONTEND = True
 BUILD_BACK = True
 RUN_BACK = True
 OPEN_FRONT = True
 
-
 print(f"Frontend build folder: {FRONTEND_BUILD_FOLDER}")
 print(f"Back public folder: {BACK_PUBLIC_FOLDER}")
 
 
-if BUILD_FRONTEND:
-    print(f"Building frontend...")
-    subprocess.check_output(["npm", "run-script", "build", "--prefix", "../frontend"])
-
-if COPY_FRONTEND:
-    print(f"Front copying...")
-    removeFolderContent(BACK_PUBLIC_FOLDER)
-    copyFolder(FRONTEND_BUILD_FOLDER, BACK_PUBLIC_FOLDER)    
-
-if BUILD_BACK:
-    print(f"Building back...")
-    subprocess.check_output(["npm", "build", "--prefix", "../backend"])
-
-if RUN_BACK:
-    print(f"Running back...")
-    subprocess.check_output(["npm", "run", "--prefix", "../backend"])
-
-if OPEN_FRONT:
-    print(f"Opening from in browser...")
-    webbrowser.open_new(SITE_URL)
-
 if RUN_DEV_SERVER:
     print(f"Running dev-server...")
     subprocess.check_output(["npm", "start", "--prefix", "../frontend"])
+
+if BUILD_FRONTEND: 
+    print(f"Building frontend...")
+    subprocess.check_output(["npm", "run-script", "build", "--prefix", "../frontend"])
+
+if COPY_FRONTEND: 
+    print(f"Front copying...")
+    copyFolder(FRONTEND_BUILD_FOLDER, BACK_PUBLIC_FOLDER)
+
+if BUILD_BACK: 
+    print(f"Building back...")
+    subprocess.check_output(["npm", "build", "--prefix", "../backend"])
+
+if RUN_BACK: 
+    print(f"Running back...")
+    subprocess.check_output(["npm", "run", "--prefix", "../backend"])
+
+if OPEN_FRONT: 
+    print(f"Opening from in browser...")
+    webbrowser.open_new(SITE_URL)
