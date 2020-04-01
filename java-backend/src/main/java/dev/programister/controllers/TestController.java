dcaaa8e069eb0dc1d94830583ff60cd853224e18
Dmitry <amld1998@mail.ru>
2020-04-01 22:14:18 +0300

    java and trash

---
+++ java-backend/src/main/java/dev/programister/controllers/TestController.java
@@ -0,0 +1,13 @@
+package dev.programister.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public  class TestController {
+//    @RequestMapping("/")
+//    @ResponseBody
+//    String hello() {
+//        return  "Hello world!!!";
+//    }
+}
---
+++ java-backend/src/main/resources/public/precache-manifest.db6f74443f9ae5db539cc359d463074f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbb51142d9f64b79f332c1866bc6238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11c8d35b08bc265069ac",
+    "url": "/static/css/main.2dbc0c2d.chunk.css"
+  },
+  {
+    "revision": "769cbb2813cdb4cc6960",
+    "url": "/static/js/2.1304a532.chunk.js"
+  },
+  {
+    "revision": "11c8d35b08bc265069ac",
+    "url": "/static/js/main.dd556528.chunk.js"
+  },
+  {
+    "revision": "d18eb385ae4a3b0a060e",
+    "url": "/static/js/runtime-main.db701789.js"
+  },
+  {
+    "revision": "9494657e0b0110a15e62bc2bdc686ef6",
+    "url": "/static/media/dashboard.9494657e.svg"
+  },
+  {
+    "revision": "4708789bdc6dca5ea884d3bf946dc252",
+    "url": "/static/media/double-arrow-right.4708789b.svg"
+  },
+  {
+    "revision": "f715585964cbe719b13915905cd297a9",
+    "url": "/static/media/email.f7155859.svg"
+  },
+  {
+    "revision": "d644ed957e2662b1d22adb890310e64f",
+    "url": "/static/media/login.d644ed95.svg"
+  },
+  {
+    "revision": "02bb06b827f09ba3b480cd923fa89a96",
+    "url": "/static/media/user.02bb06b8.svg"
+  }
+]);
