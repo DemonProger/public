919effbeac56b0ba18c18236dd6036c3e38e2897
Dmitry <amld1998@mail.ru>
2020-04-01 22:25:57 +0300

    users controller added

--- java-backend/src/main/java/dev/programister/controllers/User.java
+++ java-backend/src/main/java/dev/programister/controllers/User.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public  class TestController {
-//    @RequestMapping("/")
-//    @ResponseBody
-//    String hello() {
-//        return  "Hello world!!!";
-//    }
+public class User {
+    @RequestMapping("/users/register")
+    @ResponseBody
+    String hello() {
+        return  "Hello world!!!";
+    }
 }
