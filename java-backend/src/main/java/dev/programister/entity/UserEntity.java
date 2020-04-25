e46418acd4a6a1d11817746c06f75ab321f369af
Dmitry <amld1998@mail.ru>
2020-04-25 11:50:49 +0300

    secutiry adding

--- java-backend/src/main/java/dev/programister/entity/UserEntity.java
+++ java-backend/src/main/java/dev/programister/entity/UserEntity.java
@@ -3,8 +3,10 @@ package dev.programister.entity;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -17,14 +19,21 @@ public class UserEntity {
 
     @NotNull
     @NotEmpty
+    @Size(min = 2, message = "Too short login")
+    @Size(max = 30, message = "Too long login")
     private String login;
 
     @NotNull
     @NotEmpty
+    @Size(min = 2, message = "Too short email")
+    @Size(max = 30, message = "Too long email")
+    @Email(regexp=".@.\\..*", message = "Email should be valid")
     private String email;
 
     @NotNull
     @NotEmpty
+    @Size(min = 8, message = "Too short password")
+    @Size(max = 30, message = "Too long password")
     private String password;
 
     public UserEntity(String login, String email, String password) {
